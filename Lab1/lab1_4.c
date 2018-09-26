236ae473066e68c8a48c3c98fa9b0cfe0c245a26
Mr.Surapol Rochanapratishtha <surapol.r@rmutsb.ac.th>
2018-09-26 13:50:44 +0700

    Add files via upload

---
+++ Lab1/lab1_4.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+    printf("ID : 5210300157\t\t\t\tphone : 0636147923\n");
+    printf("Name : Surapol Rochanapratishtha\tprovince : Suphanburi\n");
+    return 0;
+}
---
+++ Lab2/lab2_1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    printf("one\ttwo\tthree\tfour\tfive\tFINISH\n");
+    printf("\tsix\tseven\teight\tnine\tten\tFINISH\n");
+    printf("123456789012345678901234567890");
+    printf("123456789012345678901234567890");
+    printf("123456789012345678901234567890");
+    printf("\n\n\n");
+    return 0;
+}
